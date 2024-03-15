b0b9078c78f48c1c147608a4106ff86cbbb65035
Shubham jha <sjha870054@gmail.com>
2024-03-15 23:56:40 +0530

    addButton added

--- 11-NotesList/src/App.jsx
+++ 11-NotesList/src/App.jsx
@@ -1,10 +1,5 @@
 import React, { useState } from 'react'
 import './index.css'
-import Logo from './components/Logo'
-import Form from './components/Form'
-import PackingList from './components/PackingList'
-import Stats from './components/Stats'
-
 const [items, setItems] = useState("")
 
 function handleAddItems(item) {
@@ -19,6 +14,88 @@ function App() {
       <PackingList items={items}/>
       <Stats/>
     </>
+  )
+}
+function Form({ onAddItems }) {
+
+  const [description, setDescription] = useState("")
+  const [quantity, setQuantity] = useState(1)
+
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem)
+
+    onAddItems(newItem);
+
+    setDescription("");
+    setQuantity(1);
+  }
+  return (
+    <form className='add-form' onSubmit={handleSubmit}>
+      <h3>What do you need for your 😍 trip?</h3>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map
+          ((num) => (
+            <option value={num} key={num} >
+              {num}
+            </option>
+          ))}
+      </select>
+      <input
+        type='text'
+        placeholder='item...'
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}>
+      </input>
+      <button>Add</button>
+    </form>
+  )
+}
+
+function PackingList({ items }) {
+  return (
+    <div className='list'>
+      <ul>
+        {items.map((item) => (
+          <Item 
+          item={item} 
+          key={item.description} 
+          />
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+function Stats(){
+  return(
+      <footer className='stats'>
+      <em>👜 You have X items on your list, and you already packed X (X%)</em>
+      </footer>
+  )
+}
+function Logo(){
+  return(
+    <>
+      <h1>🌴 Far Away 👜</h1>
+    </>
+  )
+}
+function Item({ item }) {
+  return (
+    <li>
+      <span>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
   )
 }
 
